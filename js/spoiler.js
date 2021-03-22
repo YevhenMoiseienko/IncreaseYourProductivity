37bc7101a86f5a6937a3fa95c78d65f274740d65
Yevhen Moiseienko <mojseenko2011@gmail.com>
2021-03-22 19:17:01 +0200

    no message

--- js/spoiler.js
+++ js/spoiler.js
@@ -1,6 +1,28 @@
 $(document).ready(function() {
+    slider();
+    headerSlider();
+    burger();
+});
+
+function slider(){
     $('.questions__question').click(function (event) {
         $(this).toggleClass('active').next().slideToggle(300);
     });
-});
+}
 
+function headerSlider() {
+    $('.header__name').click(function(event) {
+        if($('.header__nav').hasClass('one')) {
+            $('.header__name').not($(this)).removeClass('active');
+            $('.header__elements').not($(this).next()).slideUp(300);
+        }
+        $(this).toggleClass('active').next().slideToggle(300);
+    })
+}
+
+function burger() {
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__nav').toggleClass('active');
+        $('body').toggleClass('lock');
+    })
+}
